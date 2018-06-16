d1d90af7d845133f3df820be395cd3d3574c3ae2
Junkyu Park <junk3843@naver.com>
2018-06-16 18:50:57 +0900

    Adjust JUnit framework

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,27 +1,16 @@
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
 import com.eminentstar.junitinaction.Calculator;
 
 public class CalculatorTest {
-  private int nbErrors = 0;
+
+  @Test
   public void testAdd() {
     Calculator calculator = new Calculator();
     double result = calculator.add(10, 50);
-
-    if (result != 60) {
-      throw new IllegalStateException("Bad result: " + result);
-    }
-
+    assertEquals(60, result, 0);
   }
 
-  public static void main(String[] args) {
-    CalculatorTest test = new CalculatorTest();
-    try {
-      test.testAdd();
-    } catch (Throwable e) {
-      test.nbErrors++;
-      e.printStackTrace();
-    }
-    if (test.nbErrors > 0) {
-      throw new IllegalStateException("There were " + test.nbErrors + " error(s)");
-    }
-  }
 }
