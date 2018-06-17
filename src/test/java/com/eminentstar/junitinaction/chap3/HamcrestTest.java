9a40b6427538c21f22ed4d181be120a4ab287bf0
Junkyu Park <junk3843@naver.com>
2018-06-17 13:19:10 +0900

    (Before using Hamcrest)vague and complex test code

---
+++ src/test/java/com/eminentstar/junitinaction/chap3/HamcrestTest.java
@@ -0,0 +1,28 @@
+package com.eminentstar.junitinaction.chap3;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HamcrestTest {
+  private List<String> values;
+
+  @Before
+  public void setUpList() {
+    values = new ArrayList<String>();
+    values.add("x");
+    values.add("y");
+    values.add("z");
+  }
+
+  @Test
+  public void testWithoutHamcrest() {
+    assertTrue(values.contains("one")
+      || values.contains("two")
+      || values.contains("three"));
+  }
+}
